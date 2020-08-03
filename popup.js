a8ba262c630e2f87ccb3a6b241b75fdad13e0d90
Coul33t <Coulis1990@gmail.com>
2020-08-03 18:21:58 +0200

    :bug: :fire: Fixed an error when trying to download the same sheet multiple times + code cleaning

--- popup.js
+++ popup.js
@@ -5,20 +5,3 @@ chrome.extension.onRequest.addListener(function(link) {
         function(id) {
     });
 });
-/*document.addEventListener("DOMContentLoaded", function() {
-    var html = document.getElementsByTagName('html')[0];
-    var text = html.innerHTML;
-    alert(text);
-    let elements = document.getElementsByTagName("div");
-    alert("Number of elements found: " + elements.length);
-    if (elements.length > 0) {
-        for (element in elements) {
-            if (element.value.includes("svg")) {
-                alert("Found sheet!");
-            } else {
-                alert(element.value);
-            }
-        }
-    }
-});*/
-
