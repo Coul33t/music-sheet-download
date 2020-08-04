87fb364914a36d5d6f034b92257322988059be58
Coul33t <Coulis1990@gmail.com>
2020-08-04 15:58:48 +0200

    :bug: :sparkles: Now able to download multiple sheets (more than 2)

--- contentScript.js
+++ contentScript.js
@@ -1,16 +1,17 @@
 {
     let elements = document.getElementsByTagName("img");
-    let sheet_link;
+    let first_sheet_link = "";
 
     if (elements.length > 0) {
         for (i = 0; i < elements.length; i++) {
             let src = elements[i].getAttribute("src");
-            if (src && src.includes("svg")) {
-                sheet_link = src;
+            if (src && (src.includes("svg") || src.includes("png"))) {
+                first_sheet_link = src.substring(0, src.indexOf('?'));
+                break;
             }
         }
     }
 
     let sheet_name = document.getElementsByTagName("h1");
-    chrome.extension.sendRequest([sheet_link, sheet_name[0].innerHTML]);
+    chrome.extension.sendRequest([first_sheet_link, sheet_name[0].innerHTML]);
 }
--- popup.js
+++ popup.js
@@ -1,7 +1,29 @@
 chrome.tabs.executeScript(null,{file: "contentScript.js"});
 
 chrome.extension.onRequest.addListener(function(link) {
-    chrome.downloads.download({url: link[0], filename: link[1] + ".svg"},
-        function(id) {
-    });
+    let src = link[0];
+    let extension = src.substring(src.indexOf('.', src.length - 5));
+    let base_url = src.substring(0, src.indexOf('.', src.length - 5)).slice(0, -1);
+    let url_to_download;
+
+    for (i = 0; i < 100; i++) {
+        url_to_download = base_url + i + extension;
+
+        if (!checkURL(url_to_download)) {
+            break;
+        }
+
+        chrome.downloads.download({url: url_to_download, filename: link[1] + "_" + i + extension},
+            function(id) {
+
+            }
+        );
+    }
 });
+
+function checkURL(url) {
+    var reader = new XMLHttpRequest();
+    reader.open('HEAD', url, false);
+    reader.send();
+    return reader.status != 404;
+}
